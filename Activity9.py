dba0ad6d8de08b7514a45fd598f3554968e98979
riviel <81639665+rivielatl@users.noreply.github.com>
2026-09-07 10:45:53 +0800

    Activity 9

---
+++ Activity9.py
@@ -0,0 +1,28 @@
+# RELATIONAL OPERATOR
+
+x = eval(input(">> Give me the value of x: "))
+y = eval(input(">> Give me the value of y: "))
+
+print("____________________________________")
+print("Relational/s: \n")
+
+print("\tX is greater than Y: ", x > y)
+print("\tX is less than Y: ", x < y)
+print("\tX is greater than or equals to Y: ", x >= y)
+print("\tX is less than or equals to Y: ", x <= y)
+print("\tX is not equals to Y: ", x != y)
+print("\tX is equals to Y: ", x == y)
+
+print(" \n ____________________________________")
+
+a = input(">> Give me the word for a: ")
+b = input(">> Give me the word for b: ")
+
+print("____________________________________")
+print("Relational/s: \n")
+
+print("\tA is not equals to B: ", a != b)
+print("\tA is equals to B: ", a == b)
+
+print(" \n ____________________________________")
+
