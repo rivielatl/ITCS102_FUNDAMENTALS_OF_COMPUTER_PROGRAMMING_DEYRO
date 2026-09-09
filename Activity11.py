446653a41f2c0301b1e596cf2596a71be89e8d62
riviel <81639665+rivielatl@users.noreply.github.com>
2026-09-09 10:54:47 +0800

    Update Activity11.py

--- Activity11.py
+++ Activity11.py
@@ -1,1 +1,20 @@
+# demo
+import getpass
+
+print("<---- Register your account ---->")
+username = input("Register your username: ")
+password = input("Input your password: ")
+
+print("\n----> Log-In System <----")
+lusername = input("Username: ")
+lpassword = getpass.getpass("Password: ")
+
+if lusername == username and lpassword == password:
+	print("\n---> You have successfully LOGGED IN! <---")
+	print("Welcome to ACTIVITIY 10! :>")
+	print("User: ", username)
+else:
+	print("\nAccess DENIED!")
+	print("Please run the code to try again")
+	
 
