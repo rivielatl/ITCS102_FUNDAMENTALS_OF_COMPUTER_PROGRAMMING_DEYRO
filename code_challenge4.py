1b9ebd7d18b4aa22c473711b17b896821fd58006
riviel <81639665+rivielatl@users.noreply.github.com>
2026-09-16 11:11:13 +0800

    Code_Challenge4.py

---
+++ code_challenge4.py
@@ -0,0 +1,65 @@
+# ACTIVITY 13 LOAN [CODE CHALLENGE 4 BETTER LOANING]
+
+acc_username = "utanganph"
+password = "utangna123"
+
+loguser = input("Username: --> ")
+logpass = input("Password: --> ")
+print("________________________________________________")
+
+if loguser == acc_username and logpass == password:
+    firstname = input("What is your first name? --> ")
+    jobdescript = input("What is your job? (Include job description if you have job) --> ")
+    age = int(input("Enter your age --> "))
+    is_employed = bool(input("Are you employed? (True/Leave it Blank) --> "))
+    cred_score = eval(input("What is your credit score? (0-1000) --> "))
+    annual_income = eval(input("What is your annual income? --> "))
+    print("________________________________________________")
+    has_collateral = bool(input("Do you have any collateral? (True/Leave it blank) --> "))
+
+    if has_collateral == True:
+        print("________________________________________________")
+        item_collateral = input("What item is your collateral? --> ")
+        value_collateral = eval(input("How much value is your collateral? --> "))
+
+        if value_collateral >= 30000:
+            has_collateral = True
+        else:
+            has_collateral = False
+    else:
+        item_collateral = "None"
+
+    print("________________________________________________")
+    if (age >= 21 and age <= 65) and is_employed == True:
+        if cred_score >= 750:
+            if annual_income >= 100000:
+                inz = 0.045
+            else:
+                inz = 0.05
+        elif 600 <= cred_score < 750:
+            if has_collateral == True:
+                inz = 0.07
+            elif annual_income < 40000:
+                inz = 0.095
+            elif has_collateral == True and annual_income < 40000:
+                inz = 0.080
+            else:
+                inz = 0.08
+        else:
+            print("Rejected: Credit score too low.")
+            exit()
+    else:
+        print("Rejected: Fails baseline criteria.")
+        exit()
+        
+    print("________________________________________________\n")
+    loan = eval(input("How much do you want to loan? --> "))
+    print("\n________________________________________________\n")
+    print("Money loaned: ₱", loan)
+    print("Interest: ", inz, "%")
+    print("Loan with Interest: ₱", loan + (loan * inz))
+    print("Item Collateral: ", item_collateral)
+else:
+    print("Account username/password is incorrect.")
+
+print("________________________________________________")
